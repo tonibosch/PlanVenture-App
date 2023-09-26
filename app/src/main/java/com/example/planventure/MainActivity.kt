60f8da95906a47fcd5aebb5643186f0213f3aa43
tonibosch <tboschcastells@gmail.com>
2023-09-26 14:13:13 +0200

    Add Hide ActionBar

--- app/src/main/java/com/example/planventure/MainActivity.kt
+++ app/src/main/java/com/example/planventure/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide()
 
         val navView: BottomNavigationView = binding.navView
 
