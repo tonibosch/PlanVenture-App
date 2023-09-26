a47535266e3ce54b445ff747bd4cdbf93181acc8
tonibosch <tboschcastells@gmail.com>
2023-09-26 13:16:14 +0200

    Merge to Develop

--- app/src/main/java/com/example/planventure/MainActivity.kt
+++ app/src/main/java/com/example/planventure/MainActivity.kt
@@ -32,6 +32,4 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
-
-
 }
