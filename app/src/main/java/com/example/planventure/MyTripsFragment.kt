91f1fe186414017e3759aee647fbaf6b5cdc287e
tonibosch <tboschcastells@gmail.com>
2023-10-08 11:59:21 +0200

    Refactor Create a trip functional frontend

--- app/src/main/java/com/example/planventure/MyTripsFragment.kt
+++ app/src/main/java/com/example/planventure/MyTripsFragment.kt
@@ -22,7 +22,6 @@ import com.example.planventure.service.TripService
 @RequiresApi(Build.VERSION_CODES.P)
 class MyTripsFragment : Fragment() {
 
-
     private lateinit var button: Button
     private lateinit var scrollLayout: LinearLayout
 
@@ -33,72 +32,48 @@ class MyTripsFragment : Fragment() {
         private const val CREATE_TRIP_REQUEST = 1 // You can choose any integer value
     }
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val view = inflater.inflate(R.layout.fragment_my_trips, container, false)
         tripService = TripService(view.context)
 
-
         button = view.findViewById(R.id.button2)
-
         button.setOnClickListener {
             val intent = Intent(container!!.context, CreateTripActivity::class.java)
             startActivityForResult(intent, CREATE_TRIP_REQUEST)
         }
-
-        //refreshTripList()
-        val trips = tripService.getAllTrips()
-
-        val linearLayout = view?.findViewById<LinearLayout>(R.id.linearLayout)
-        linearLayout?.removeAllViews() // Clear existing views
-
-        var i = 1
-        for (trip in trips) {
-            val textView = TextView(context)                                // Create a new TextView for each trip
-            textView.text = "Trip $i\n      Name: ${trip.getName()}\n       Location: ${trip.getLocation()}"
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
-
-            if (i % 2 == 0) textView.setBackgroundColor(Color.WHITE)        // Set the background color based on whether i is odd or even
-            else textView.setBackgroundColor(Color.LTGRAY)
-            linearLayout?.addView(textView)
-            ++i
-        }
-
         return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        refreshTripList()
+    }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == CREATE_TRIP_REQUEST && resultCode == Activity.RESULT_OK) {
-            // A new trip has been created, refresh the list of trips
-            refreshTripList()
+            refreshTripList()           //When a new trip has been created, refresh the list of trips
         }
     }
 
     private fun refreshTripList() {
         val trips = tripService.getAllTrips()
-
         val linearLayout = view?.findViewById<LinearLayout>(R.id.linearLayout)
-        linearLayout?.removeAllViews() // Clear existing views
 
         var i = 1
         for (trip in trips) {
             val textView = TextView(context)                                // Create a new TextView for each trip
-            textView.text = "Trip $i\n      Name: ${trip.getName()}\n       Location: ${trip.getLocation()}"
+            textView.text = "Trip $i\n       Name: ${trip.getName()}\n       Location: ${trip.getLocation()}"
             textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18f)
-
             if (i % 2 == 0) textView.setBackgroundColor(Color.WHITE)        // Set the background color based on whether i is odd or even
             else textView.setBackgroundColor(Color.LTGRAY)
             linearLayout?.addView(textView)
             ++i
         }
     }
-
-
-
 }
