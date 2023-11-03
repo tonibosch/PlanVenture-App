08b04e74813dfe6fb85233b023cbcf14c50d4f70
TxRuX <maharneu@gmail.com>
2023-11-03 18:46:48 +0100

    refactoring

--- app/src/main/java/com/example/planventure/interfaces_abstracts/CRUDSQLliteRepository.kt
+++ app/src/main/java/com/example/planventure/interfaces_abstracts/CRUDSQLliteRepository.kt
@@ -45,7 +45,10 @@ abstract class CRUDSQLiteRepository<T>(context: Context, private val table: Stri
          * - array of where arguments that are placed behind the where clause
          * when the operation was successful return true else return false
          */
-        return when(wdb.update(TRIP_TABLE, cv, "ID=?", arrayOf(id.toString()))) {-1 -> false else -> true}
+        return when(wdb.update(TRIP_TABLE, cv, "ID=?", arrayOf(id.toString()))) {
+            -1  -> false
+            else -> true
+        }
     }
 
     override fun delete(q: String): Boolean{
